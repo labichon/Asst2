090a88dc26cf8a4e6303ed016f1a1337b8e48066
Tarek Z. Mohammed <tzm6@ilab1.cs.rutgers.edu>
2020-11-25 14:30:51 -0500

    added more test stuff and continued on the directory handle function

--- Asst2.c
+++ Asst2.c
@@ -4,28 +4,55 @@
 #include <pthread.h>
 #include <dirent.h>
 #include <errno.h>
+#include <string.h>
+
+//there should be a test directory too
+//theres directory and a txt file in that
+//directory
 
 struct list {
-	char* id;
-	int count; //maybe not necessary, just here for now
+	char* token;
+	int prob;
 	struct list *next;
 };
 
+//we need to pass a struct of info into the void function
+//im sure as we move further along, more stuff will be added
 struct parameters {
 	char* directname;
 	
 };
 
 
-void *directhandle(void *arg){
+void *directhandle(void *holder){
 	
-	struct parameters *dirarg = arg;
-	DIR* dir = opendir(dirarg->directname);
+	struct parameters *argument = holder;
+	DIR* dir = opendir(argument->directname);
 	struct dirent *dp;
 
-	while((dp = readdir(dir))){
-		printf("aids\n");
+	while((dp = readdir(dir))){	
+		if(!strcmp(dp->d_name, ".")){
+			continue;
+		}
+		if(!strcmp(dp->d_name, "..")){
+			continue;
+		}
+		if(dp->d_type == DT_REG){
+			//printf("aids3\n");
+		}
+	       	if(dp->d_type == DT_DIR){			
+			struct parameters* dirargument = (struct parameters*) malloc(sizeof(struct parameters));
+                	//need to find a way to put the name of this directory
+			//into the parameters struct to pass to another pthread
+			/*dirargument->directname = directoryname;
+                	pthread_t direct;
+                	pthread_create(&direct, NULL, directhandle, argument);
+                	pthread_join(direct, &rval);*/
+		}
+		
 	}
+	closedir(dir);
+	
 	
 }
 
@@ -36,6 +63,9 @@ int main(int argc, char *argv[]){
 	DIR* dir = opendir(directoryname);
 	void *rval; //return value
 
+	//the assignment says to check a valid directory in the main,
+	//but also in the directory-handling function
+	//i put it the checking here for now
 	if (dir) {
 		struct parameters* argument = (struct parameters*) malloc(sizeof(struct parameters));
 		argument->directname = directoryname;
